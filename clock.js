601e8179b02eeee3d2e979afb06fbba24d64a2ea
Devin Jakubowicz <devinjakub@gmail.com>
2016-11-17 14:19:43 -0500

    added function startTime and functions changeBackground to the js file

--- clock.js
+++ clock.js
@@ -1,10 +1,37 @@
 function startTime() {
 
-	var today = 
+	var today = newDate();
 
 	var hours = today.getHours();
 
 	var mins = today.getMinutes();
 
 	var secs = today.getSeconds();
+
+changeBackground(hours, mins, secs);
+
+mins = checkTime(mins);
+
+secs = checkTime(secs);
+
+document.getElementsByClassName("clock")[0].innerHTML = hours+":"mins+":"+secs;
+
+setTimeout(function(){startTime()}, 100);
+
+}
+
+function changeBackground(hours,mins,secs){
+
+var totalMins = (hours * 60) + mins;
+
+var huecolor = totalMins * 0.25;
+
+var saturation = mins + 10;
+
+var lightness = secs + 5;
+
+document.body.style.backgroundColor= 
+
+
+
 }
