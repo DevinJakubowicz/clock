022f631981c6eeaad4dbcd63f984d631599df151
Devin Jakubowicz <devinjakub@gmail.com>
2016-11-17 15:52:25 -0500

    added js for clock to change the color with hex codes

--- clock.js
+++ clock.js
@@ -1,37 +1,79 @@
-function startTime() {
-
-	var today = newDate();
-
-	var hours = today.getHours();
-
-	var mins = today.getMinutes();
-
-	var secs = today.getSeconds();
-
-changeBackground(hours, mins, secs);
-
-mins = checkTime(mins);
-
-secs = checkTime(secs);
-
-document.getElementsByClassName("clock")[0].innerHTML = hours+":"mins+":"+secs;
-
-setTimeout(function(){startTime()}, 100);
-
-}
-
-function changeBackground(hours,mins,secs){
-
-var totalMins = (hours * 60) + mins;
-
-var huecolor = totalMins * 0.25;
-
-var saturation = mins + 10;
-
-var lightness = secs + 5;
-
-document.body.style.backgroundColor= 
+document.getElementById("myClock").addEventListener("load", DevinsClock)
 
 
+function DevinsClock() {
+
+
+	var today = new Date();
+
+	var hour = today.getHours();
+
+	var min = today.getMinutes();
+
+	var sec = today.getSeconds();
+
+if(hour <= 9) hour = '0'+hour;
+
+if(min <= 9) min = '0'+min;
+
+if(sec <= 9) sec = '0'+sec;
+
+var color = "#"+hour+min+sec;
+
+document.body.style.background = color;
+
+document.getElementById("clock").innerHTML = color;
+
+setTimeout(DevinsClock, 1000);
+
+document.getElementsByTagName("h1")[0].innerText = color;
 
 }
+
+
+DevinsClock()
+
+
+
+
+
+// document.getElementById("myClock").addEventListener("load", DevinsClock)
+
+
+// function DevinsClock() {
+
+// 	var today = new Date();
+
+// 	var hour = today.getHours();
+
+// 	var min = today.getMinutes();
+
+// 	var sec = today.getSeconds();
+
+// changeBackground(hour, min, sec);
+
+// min = checkTime(min);
+
+// sec = checkTime(sec);
+
+// document.getElementById("clock").innerHTML = hour+ ":" min+ ":" +sec;
+
+// setTimeout(function(){startTime()}, 100);
+
+// }
+
+// function changeBackground(hour,min,sec){
+
+// var totalMins = (hour * 60) + min;
+
+// var huecolor = totalMins * 0.25;
+
+// var saturation = min + 10;
+
+// var lightness = sec + 5;
+
+// document.body.style.backgroundColor= "hsl(" + huecolor + ", " + saturation + "%, " + lightness + "%)";
+
+// }
+
+// DevinsClock()
